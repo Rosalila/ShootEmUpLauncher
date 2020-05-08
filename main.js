e4756bf2e8e78943829732110a339e254eae2b27
turupawn <ahmed.hn.43@gmail.com>
2020-05-08 00:09:03 -0600

    input config

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const {app, BrowserWindow, Menu} = electron
+const {app, BrowserWindow, Menu, ipcMain} = electron
 
 //process.env.NODE_ENV = 'production'
 
@@ -10,6 +10,8 @@ let mainWindow
 
 app.on('ready', function(){
   mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
     webPreferences: {
       nodeIntegration: true
     }
@@ -26,6 +28,58 @@ app.on('ready', function(){
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
   Menu.setApplicationMenu(mainMenu)
 })
+
+function createConfigureKeyboardWindow()
+{
+  configureKeyboardWindow = new BrowserWindow({
+    width: 300,
+    height: 600,
+    title: 'Configure Keyboard',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  configureKeyboardWindow.loadURL(
+    url.format({
+        pathname: path.join(__dirname, 'configureKeyboard.html'),
+        protocol: 'file',
+        slashes: true
+  }))
+
+  configureKeyboardWindow.on('close', function(){
+    configureKeyboardWindow = null
+  })
+}
+
+function createConfigureGamepadWindow()
+{
+  configureGamepadWindow = new BrowserWindow({
+    width: 300,
+    height: 600,
+    title: 'Configure Gamepad',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  configureGamepadWindow.loadURL(
+    url.format({
+        pathname: path.join(__dirname, 'configureGamepad.html'),
+        protocol: 'file',
+        slashes: true
+  }))
+
+  configureGamepadWindow.on('close', function(){
+    configureGamepadWindow = null
+  })
+}
+
+ipcMain.on('create-configure-keyboard-window', (event) => {
+  createConfigureKeyboardWindow()
+});
+
+ipcMain.on('create-configure-gamepad-window', (event) => {
+  createConfigureGamepadWindow()
+});
 
 const mainMenuTemplate = [
   {
